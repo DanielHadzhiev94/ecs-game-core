9c6ac2baa37808afbc83177d08871cf637242418
Daniel Hadzhiev <145060639+DanielHadzhiev94@users.noreply.github.com>
2026-01-15 20:16:36 +0100

    Implemented base lifetime system (#8)
    - Created health component
    - Mark the component for destruction, if the value 0 or less

---
+++ src/game/components/health.h
@@ -0,0 +1,5 @@
+#include <cstdint>
+
+struct Health {
+    std::int32_t value;
+};
---
+++ src/game/systems/lifetime_system.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "registry/registry.h"
+
+class LifetimeSystem {
+public:
+    void update(Registry &registry, float dt);
+};
