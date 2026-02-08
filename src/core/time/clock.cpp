3fbaf02d1f9f2031bc55cc360d8073f8869659e9
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-02-08 15:05:55 +0100

    Fix small typo mistakes

--- src/core/time/clock.cpp
+++ src/core/time/clock.cpp
@@ -2,17 +2,20 @@
 
 using namespace std::chrono;
 
-namespace engine::time {
-    float Clock::restart() {
+namespace engine::time
+{
+    float Clock::restart()
+    {
         auto current_frame = steady_clock::now();
         auto delta = current_frame - last_frame;
 
         last_frame = current_frame;
 
-        return duration_cast<duration<float> >(delta).count();
+        return duration_cast<duration<float>>(delta).count();
     }
 
-    void Clock::set_initial_frame() {
+    void Clock::set_initial_frame()
+    {
         last_frame = steady_clock::now();
     }
 }
