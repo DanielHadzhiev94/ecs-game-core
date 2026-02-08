7f7456b258aeeb89b0c157f21f34cde94304bd95
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-02-08 23:11:34 +0100

    Replace the systems with SystemManager in World

--- src/game/world/world.cpp
+++ src/game/world/world.cpp
@@ -1,35 +1,31 @@
 #include "world.hpp"
 
-namespace engine::game {
+namespace engine::game
+{
     constexpr float FIXED_DT = 1.0f / 60.0f;
 
     World::World(ecs::Registry &registry, render::IRenderer &renderer)
-        : registry(registry), render_system(renderer) {
+        : registry(registry), render_system(renderer)
+    {
     }
 
-    void World::tick(float dt) {
+    void World::tick(float dt)
+    {
         accumulator += dt;
 
-        while (accumulator >= FIXED_DT) {
-            fixed_update(FIXED_DT);
+        while (accumulator >= FIXED_DT)
+        {
+            system_manager.fixed_update(registry, FIXED_DT);
             accumulator -= FIXED_DT;
         }
 
         const float alpha = accumulator / FIXED_DT;
-        update(dt);
+        system_manager.update(registry, dt);
         render(alpha);
     }
 
-    void World::fixed_update(float fixed_dt) {
-        movement_system.fixed_update(registry, fixed_dt);
-        lifetime_system.fixed_update(registry, fixed_dt);
-        camera_system.fixed_update(registry, fixed_dt);
-    }
-
-    void World::update(float dt) {
-    }
-
-    void World::render(float alpha) {
+    void World::render(float alpha)
+    {
         render_system.render(registry, alpha);
     }
 }
--- src/game/world/world.hpp
+++ src/game/world/world.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
-#include "../systems/gameplay/lifetime_system.hpp"
-#include "../systems/gameplay/movement_system.hpp"
-#include "../systems/camera/camera_system.hpp"
 #include "../systems/render/render_system.hpp"
 #include "ecs/registry/registry.hpp"
-
-namespace engine::game {
-    class World {
+#include "manager/system_manager.hpp"
+namespace engine::game
+{
+    class World
+    {
     public:
         explicit World(ecs::Registry &registry, render::IRenderer &renderer);
 
@@ -17,15 +16,9 @@ namespace engine::game {
         ecs::Registry registry;
 
         systems::RenderSystem render_system;
-        systems::CameraSystem camera_system;
-        systems::MovementSystem movement_system;
-        systems::LifetimeSystem lifetime_system;
+        core::manager::SystemManager system_manager;
 
         float accumulator = 0.0f;
-
-        void fixed_update(float fixed_dt);
-
-        void update(float dt);
 
         void render(float alpha);
     };
