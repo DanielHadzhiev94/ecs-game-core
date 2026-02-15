0b26d98417b1bba1d6acbbf231b93f606ee47cd3
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-02-15 23:38:54 +0100

    Reworked component storage

--- src/core/ecs/storage/component_storage.hpp
+++ src/core/ecs/storage/component_storage.hpp
@@ -1,56 +1,92 @@
 #pragma once
+#include <vector>
 #include <unordered_map>
 #include "../entity/entity_id.hpp"
 
-namespace engine::ecs {
+namespace engine::ecs
+{
 
-    struct IComponentStorage {
+    struct IComponentStorage
+    {
         virtual ~IComponentStorage() = default;
 
-        virtual bool destroy(EntityId);
+        virtual bool destroy(EntityId) = 0;
     };
 
-    template<typename T>
-    class ComponentStorage final : public IComponentStorage {
+    template <typename T>
+    class ComponentStorage final : public IComponentStorage
+    {
     public:
-        bool create(EntityId entity) {
-            auto it = components.find(entity.index);
-            if (it != components.end())
+        template <typename... Args>
+        T &emplace(EntityId entity, Args &&...args)
+        {
+
+            std::size_t index = components_.size();
+            components_.emplace_back(std::forward<Args>(args)...);
+            index_to_entity_.push_back(entity);
+            entity_to_index_.emplace(entity.index, index);
+
+            return components_.back();
+        }
+
+        T *get(EntityId entity)
+        {
+            auto it = entity_to_index_.find(entity.index);
+            if (it == entity_to_index_.end())
+                return nullptr;
+
+            return &components_[it->second];
+        }
+
+        const T *get(EntityId entity) const
+        {
+            auto it = entity_to_index_.find(entity.index);
+            if (it == entity_to_index_.end())
+                return nullptr;
+
+            return &components_.at(it->second);
+        }
+
+        bool has(EntityId entity)
+        {
+            return entity_to_index_.find(entity.index) != entity_to_index_.end();
+        }
+
+        std::vector<T> &raw()
+        {
+            return components_;
+        }
+
+        bool destroy(EntityId entity) override
+        {
+            auto it = entity_to_index_.find(entity.index);
+            if (it == entity_to_index_.end())
                 return false;
 
-            components.emplace(entity.index, T{});
+            const std::size_t index = it->second;
+            const std::size_t last_index = components_.size() - 1;
+
+            // Swap current component with  component on the last position
+            components_[index] = std::move(components_[last_index]);
+
+            // Get the entity on the last index
+            EntityId last_entity = index_to_entity_[last_index];
+
+            // Replace the entity in the searched index, with the entity of the last element
+            // this way we can remove the last element
+            index_to_entity_[index] = last_entity;
+            entity_to_index_[index] = last_entity.index;
+
+            components_.pop_back();
+            index_to_entity_.pop_back();
+            entity_to_index_.erase(it);
 
             return true;
         }
 
-        T *get(EntityId entity) {
-            auto it = components.find(entity.index);
-            if (it == components.end())
-                return nullptr;
-
-            return &it->second;
-        }
-
-        const T *get(EntityId entity) const {
-            auto it = components.find(entity.index);
-            if (it == components.end())
-                return nullptr;
-
-            return &it->second;
-        }
-
-        bool destroy(EntityId entity) override {
-            auto it = components.find(entity.index);
-            if (it != components.end()) {
-                components.erase(entity.index);
-
-                return true;
-            }
-
-            return false;
-        }
-
     private:
-        std::unordered_map<std::size_t, T> components;
+        std::vector<T> components_;
+        std::vector<EntityId> index_to_entity_;
+        std::unordered_map<std::size_t, std::size_t> entity_to_index_;
     };
 }
