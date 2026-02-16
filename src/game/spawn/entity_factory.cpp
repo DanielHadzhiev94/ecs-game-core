1e90f96479e0e294f228389a379e4b29bb582a46
Daniel Hadzhiev <daniel.hadzhiev@outlook.com>
2026-02-16 22:42:37 +0100

    reworked entity factory
    - Add more components to init_character method

--- src/game/spawn/entity_factory.cpp
+++ src/game/spawn/entity_factory.cpp
@@ -1,5 +1,7 @@
 #include "entity_factory.hpp"
 
+#include "../components/collider.hpp"
+#include "../components/health.hpp"
 #include "../components/position.hpp"
 #include "../components/velocity.hpp"
 #include "../components/player_tag.hpp"
@@ -26,35 +28,31 @@ namespace engine::game::spawn
         return e;
     }
 
-    void EntityFactory::init_character(ecs::EntityId e, components::Position position,
-                                       CharacterType character_type)
+    void EntityFactory::init_character(
+        ecs::EntityId e,
+        components::Position position,
+        CharacterType character_type)
     {
+        if (character_type == CharacterType::Player)
+            registry.add<components::PlayerTag>(e);
+        else
+            registry.add<components::EnemyTag>(e);
+
+        registry.add<components::Position>(e, position);
+        registry.add<components::Velocity>(e, components::Velocity{0.f, 0.f});
+
+        // Collider
+        components::Collider collider{};
 
         if (character_type == CharacterType::Player)
-        {
-            registry.add<components::PlayerTag>(e);
-        }
+            collider.half_extends = {16.f, 16.f};
         else
-        {
-            registry.add<components::EnemyTag>(e);
-        }
+            collider.half_extends = {14.f, 14.f};
 
-        registry.add<components::Position>(e);
-        registry.add<components::Velocity>(e);
+        registry.add<components::Collider>(e, collider);
 
-        auto *p = registry.get<components::Position>(e);
-        if (p != nullptr)
-        {
-            p->current;
-        }
-
-        auto *v = registry.get<components::Velocity>(e);
-        if (v != nullptr)
-        {
-            // TODO: Velocity maybe should not be vector!!!
-            auto velocity = character_type == CharacterType::Player ? 1.3F : 1.1F;
-            v->current.x = velocity;
-            v->current.y = velocity;
-        }
+        // Health
+        int health = character_type == CharacterType::Player ? 100 : 50;
+        registry.add<components::Health>(e, components::Health{health});
     }
 }
--- src/game/spawn/entity_factory.hpp
+++ src/game/spawn/entity_factory.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include "ecs/registry/registry.hpp"
 
+namespace engine::game::components
+{
+    struct Position;
+}
+
 namespace engine::game::spawn
 {
     enum class CharacterType
@@ -14,13 +19,13 @@ namespace engine::game::spawn
     public:
         explicit EntityFactory(ecs::Registry &);
 
-        ecs::EntityId create_player(components::Position);
+        ecs::EntityId create_player(components::Position position);
 
-        ecs::EntityId create_enemy(components::Position);
+        ecs::EntityId create_enemy(components::Position position);
 
     private:
         ecs::Registry &registry;
 
-        void init_character(ecs::EntityId, components::Position position, CharacterType character_type);
+        void init_character(ecs::EntityId, components::Position, CharacterType);
     };
 }
